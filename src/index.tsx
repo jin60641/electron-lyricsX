7eb67f24b01f9010c5963c2536f88185ba05c5b4
jinsang <jin60641@gmail.com>
2021-10-01 07:10:21 +0900

    add i18n base

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
@@ -8,15 +9,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { persistor, store } from './store';
+import { initializeI18next } from './utils/i18next';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={<></>} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root'),
-);
+const { locale: { code } } = store.getState();
+
+(async () => {
+  const i18n = await initializeI18next(code);
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <PersistGate loading={<></>} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>,
+    document.getElementById('root'),
+  );
+})();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
