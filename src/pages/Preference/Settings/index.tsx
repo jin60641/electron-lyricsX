d0f5bbf5e64e379f4f2a9979a8f79747ca39113f
g1moon <gmoon.cho@gmail.com>
2021-10-06 21:15:33 +0900

    feat: preference-setting에서 draggable의 상태를 "ON", "OFF"로 설정하는 기능 추가.

--- src/pages/Preference/Settings/index.tsx
+++ src/pages/Preference/Settings/index.tsx
@@ -1,30 +1,58 @@
 import React, { useCallback, useState } from 'react';
 
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import { useDispatch } from 'react-redux';
 
+import layoutAction from 'store/layout/actions';
 import actions from 'store/music/actions';
 
 const Settings: React.FC = () => {
   const dispatch = useDispatch();
   const [offset, setOffset] = useState(-0.5);
+  const [draggable, setDraggable] = useState('ON');
 
   const handleOnChange = useCallback((e) => {
     dispatch(actions.setGlobalOffset(e.target.value));
     setOffset(() => Number(e.target.value));
   }, [dispatch]);
+
+  const handleOnChangeDraggable = useCallback((e) => {
+    dispatch(layoutAction.setDraggable(e.target.value));
+    setDraggable(() => String(e.target.value));
+  }, [dispatch]);
+
   return (
-    <TextField
-      type='number'
-      value={offset}
-      label='Offset'
-      variant='outlined'
-      inputProps={{
-        maxLength: 13,
-        step: '0.1',
-      }}
-      onChange={handleOnChange}
-    />
+    <>
+      <TextField
+        type='number'
+        value={offset}
+        label='Offset'
+        variant='outlined'
+        inputProps={{
+          maxLength: 13,
+          step: '0.1',
+        }}
+        onChange={handleOnChange}
+      />
+      <InputLabel id='demo-simple-select-autowidth-label'>
+        Draggable
+      </InputLabel>
+      <Select
+        labelId='demo-simple-select-autowidth-label'
+        id='demo-simple-select-autowidth'
+        value={draggable}
+        onChange={handleOnChangeDraggable}
+        autoWidth
+        label='Draggable'
+        defaultValue={draggable}
+      >
+        <MenuItem value='ON'>ON</MenuItem>
+        <MenuItem value='OFF'>OFF</MenuItem>
+      </Select>
+    </>
   );
 };
 
