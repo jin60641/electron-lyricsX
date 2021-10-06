88887c122e7f399785a0e55471cb71ecae6c7ad6
g1moon <gmoon.cho@gmail.com>
2021-10-06 21:43:15 +0900

    feat: SET_DRAGGABLE 타입 추가.

--- src/store/layout/types.ts
+++ src/store/layout/types.ts
@@ -1,10 +1,13 @@
 export interface LayoutState {
   palette: Palette;
+  draggable: Draggable;
 }
 
 export enum Actions {
   SET_PALETTE = 'LAYOUT.SET_PALETTE',
   CLOSE_PREFERENCE = 'LAYOUT.CLOSE_PREFERENCE',
+
+  SET_DRAGGABLE = 'LAYOUT.SET_DRAGGABLE',
 }
 
 export enum Palette {
@@ -13,4 +16,9 @@ export enum Palette {
   LIGHT = 'light',
 }
 
-export const initialState: LayoutState = { palette: Palette.DEVICE };
+export enum Draggable {
+  ON = 'ON',
+  OFF = 'OFF',
+}
+
+export const initialState: LayoutState = { palette: Palette.DEVICE, draggable: Draggable.ON };
