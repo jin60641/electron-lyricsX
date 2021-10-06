dad01f33845dc10dc4d889153bc568ab2a3b341b
g1moon <gmoon.cho@gmail.com>
2021-10-06 21:06:36 +0900

    Feat: SET_DRAGGABLE 액션 추가.

--- src/store/layout/actions.ts
+++ src/store/layout/actions.ts
@@ -4,5 +4,6 @@ import { Actions, LayoutState } from './types';
 
 const setPalette = createAction(Actions.SET_PALETTE)<LayoutState['palette']>();
 const closePreference = createAction(Actions.CLOSE_PREFERENCE)();
+const setDraggable = createAction(Actions.SET_DRAGGABLE)<LayoutState['draggable']>();
 
-export default { setPalette, closePreference };
+export default { setPalette, closePreference, setDraggable };
