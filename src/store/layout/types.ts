14018803b0f44f44af416a4bbf1d73011c8aa7ed
ttubuckee <tommy1003@naver.com>
2021-10-07 12:50:31 +0900

    wip
    > lazy import 에러

--- src/store/layout/types.ts
+++ src/store/layout/types.ts
@@ -1,10 +1,13 @@
 export interface LayoutState {
   palette: Palette;
+
 }
 
 export enum Actions {
   SET_PALETTE = 'LAYOUT.SET_PALETTE',
   CLOSE_PREFERENCE = 'LAYOUT.CLOSE_PREFERENCE',
+
+  CLOSE_SEARCH_MODAL = 'LAYOUT.CLOSE_SEARCH_MODAL',
 }
 
 export enum Palette {
