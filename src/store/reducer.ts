33ae177a34e13f6c1833f3796853c039a0e6594b
ttubuckee <tommy1003@naver.com>
2021-10-12 15:18:25 +0900

    refactor: search reducer 추가
    누락된 searchReducer import 후 combineReducers에 추가

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux';
 import layout from './layout/reducer';
 import locale from './locale/reducer';
 import music from './music/reducer';
+import search from './search/reducer';
 
-const rootReducer = combineReducers({ music, layout, locale });
+const rootReducer = combineReducers({ music, layout, locale, search });
 
 export default rootReducer;
