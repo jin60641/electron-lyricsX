1daa4e678398ac63c18b62e2acb10f531d5ca9aa
g1moon <gmoon.cho@gmail.com>
2021-10-13 20:36:00 +0900

    feat: preference-theme 자막 스타일 상태를 선택(Select)하는 컴포넌트 추가.

---
+++ src/components/MenuTextInput/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { TextField } from '@material-ui/core';
+
+interface Props {
+  label: string,
+  menuState: any,
+  changeHandler: (e: any) => void,
+  stepSize: number,
+}
+
+const MenuTextInput: React.FC<Props> = ({ label, menuState, changeHandler, stepSize }) => {
+  return (
+    <>
+      <TextField
+        type='number'
+        value={menuState}
+        label={label}
+        variant='outlined'
+        inputProps={{
+          maxLength: 10,
+          step: stepSize,
+        }}
+        onChange={changeHandler}
+        style={{ marginBottom: '15px' }}
+      />
+    </>
+  );
+};
+
+export default MenuTextInput;
