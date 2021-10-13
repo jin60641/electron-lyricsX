12335c90a432344aede5d79f7047278de7c8b6af
g1moon <gmoon.cho@gmail.com>
2021-10-13 20:29:01 +0900

    feat: preference-theme 자막 정보 스타일 커스텀 상태들을 관리하는 커스텀 훅을 추가합니다.

---
+++ src/hooks/useCustom.ts
@@ -0,0 +1,82 @@
+import { useCallback } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import actions from '../store/layout/actions';
+import { RootState } from '../store/types';
+
+const styleCustomSelector = ({
+  layout: {
+    palette,
+    lineCount,
+    lyricSize,
+    fontColor,
+    backgroundOpacity,
+    backgroundColor,
+  },
+}: RootState) => ({
+  palette,
+  lineCount,
+  lyricSize,
+  fontColor,
+  backgroundOpacity,
+  backgroundColor,
+});
+
+export const useThemeCustom = () => {
+  const {
+    palette,
+    lineCount,
+    lyricSize,
+    fontColor,
+    backgroundOpacity,
+    backgroundColor,
+  } = useSelector(styleCustomSelector);
+
+  const dispatch = useDispatch();
+
+  const handleChangePalette = useCallback((e) => {
+    dispatch(actions.setPalette(e.target.value));
+  }, [dispatch]);
+
+  const handleChangeLineCount = useCallback((e) => {
+    dispatch(actions.setLineCount(e.target.value));
+  }, [dispatch]);
+
+  const handleChangeFontSize = useCallback((e) => {
+    dispatch(actions.setLyricSize(e.target.value));
+  }, [dispatch]);
+
+  const handleChangeFontColor = useCallback((e) => {
+    dispatch(actions.setFontColor(e.target.value));
+  }, [dispatch]);
+
+  const handleChangeBackgroundColor = useCallback((e) => {
+    dispatch(actions.setBackgroundColor(e.target.value));
+  }, [dispatch]);
+
+  const handleChangeBackgroundOpacity = useCallback((e) => {
+    console.log(e.target, '----------------');
+    if (Number(e.target.value) < 0 || Number(e.target.value) > 1) {
+      // eslint-disable-next-line no-alert
+      alert('This value must be set from 0 to 1.');
+      return;
+    }
+    dispatch(actions.setBackgroundOpacity(e.target.value));
+  }, [dispatch]);
+
+  return {
+    palette,
+    lineCount,
+    lyricSize,
+    fontColor,
+    backgroundOpacity,
+    backgroundColor,
+    handleChangePalette,
+    handleChangeLineCount,
+    handleChangeFontSize,
+    handleChangeFontColor,
+    handleChangeBackgroundColor,
+    handleChangeBackgroundOpacity,
+  };
+};
