38d4bf0fbe62242a426ea99648c9a5a080d66bfc
ttubuckee <tommy1003@naver.com>
2021-10-19 15:30:06 +0900

    remove: search store 제거
    Nav에서 title, artist를 useState로 관리하도록 변경하여
    search 관련 store 폴더 제거

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,8 +3,7 @@ import { combineReducers } from 'redux';
 import layout from './layout/reducer';
 import locale from './locale/reducer';
 import music from './music/reducer';
-import search from './search/reducer';
 
-const rootReducer = combineReducers({ music, layout, locale, search });
+const rootReducer = combineReducers({ music, layout, locale });
 
 export default rootReducer;
--- src/store/types.ts
+++ src/store/types.ts
@@ -7,21 +7,17 @@ import localeActions from './locale/actions';
 import { LocaleState } from './locale/types';
 import musicActions from './music/actions';
 import { MusicState } from './music/types';
-import searchActions from './search/actions';
-import { SearchState } from './search/types';
 
 export interface RootState {
   music: MusicState,
   layout: LayoutState,
   locale: LocaleState,
-  search: SearchState,
 }
 
 export type RootAction =
   ActionType<typeof musicActions> |
   ActionType<typeof localeActions> |
-  ActionType<typeof layoutActions> |
-  ActionType<typeof searchActions>;
+  ActionType<typeof layoutActions>;
 
 export const channels = [
   musicActions.resetMusic,
