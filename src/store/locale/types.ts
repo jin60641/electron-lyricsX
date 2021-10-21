3b7b8042addf19a563823c3b9865a33169740bf5
g1moon <gmoon.cho@gmail.com>
2021-10-21 21:33:26 +0900

    feat: music player를 선택하는 액션 추가.

--- src/store/locale/types.ts
+++ src/store/locale/types.ts
@@ -1,9 +1,17 @@
 export enum Actions {
   SET_LOCALE = 'LOCALE.SET_LOCALE',
+  SET_PLAYER = 'LOCALE.SET_PLAYER',
 }
 
 export type LocaleState = {
   code: string;
+  player: Player;
 };
 
-export const initialState: LocaleState = { code: 'en' };
+export enum Player {
+  CHROME = 'chrome',
+  ITUNES = 'itunes',
+  CHROME_EXTENSION = 'chromeExtension',
+}
+
+export const initialState: LocaleState = { code: 'en', player: Player.CHROME };
