b5c0b6fd44d0d0e2bc00812730921f466ab983ec
g1moon <gmoon.cho@gmail.com>
2021-10-21 19:40:33 +0900

    feat: music player 선택하는 ui 추가.

--- src/pages/Preference/Settings/index.tsx
+++ src/pages/Preference/Settings/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from 'react';
 
 import {
-  Checkbox, FormControl, FormControlLabel, TextField,
+  Checkbox, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField,
 } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -37,6 +37,27 @@ const Settings: React.FC = () => {
         }}
         onChange={handleOnChange}
       />
+      <FormControl
+        component='fieldset'
+        style={{ border: '0.2px solid rgba(0, 0, 0, .3)', borderRadius: '5px', marginTop: '7px' }}
+      >
+        <FormLabel
+          component='legend'
+          style={{ fontSize: '12px', marginLeft: '9px', padding: '0 3px' }}
+        >
+          Music Player
+        </FormLabel>
+        <RadioGroup
+          aria-label='musicPlayer'
+          defaultValue='chrome'
+          name='radio-buttons-group'
+          style={{ display: 'flex', flexDirection: 'row', marginLeft: '10px', padding: '3px 10px', justifyContent: 'space-between' }}
+        >
+          <FormControlLabel value='chrome' control={<Radio color='primary' />} label='chrome(only Mac)' />
+          <FormControlLabel value='itunes' control={<Radio color='primary' />} label='itunes' />
+          <FormControlLabel value='chromeExtension' control={<Radio color='primary' />} label='chrome Extension' />
+        </RadioGroup>
+      </FormControl>
       <FormControl>
         <FormControlLabel
           control={<Checkbox defaultChecked color='primary' />}
