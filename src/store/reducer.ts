49a7f8f84f2518cca69b88c28610f1d453a734ab
g1moon <gmoon.cho@gmail.com>
2022-01-05 02:48:02 +0900

    refactor: rootReducer -> createSlice로 만들어진 리듀서로 변경 (#30)

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import layout from './layout/reducer';
 import music from './music/reducer';
-import preference from './preference/reducer';
+import preference from './preference/preferenceSlice';
 
 const rootReducer = combineReducers({ music, layout, preference });
 
