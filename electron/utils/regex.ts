d04838c699762fa9b3f2b201d57cf32f9e2f44af
jin60641 <jin60641@naver.com>
2022-12-13 16:52:40 +0900

    fix: timestamp ms 2글자 -> 3글자까지 파싱 가능하도록 수정

--- electron/utils/regex.ts
+++ electron/utils/regex.ts
@@ -1,3 +1,3 @@
-export const timeTagRegex = /\[(?:(\d\d):)?(?:([0-5]\d).)?(\d\d)\]/;
+export const timeTagRegex = /\[(?:(\d\d):)?(?:([0-5]\d).)?(\d\d\d?)\]/;
 
 export const filterRegex = /(by|title|song|album|artist|singer|lyrics|词|曲|作詞|作词|作曲|編曲|编曲|収録|收录|演唱|歌手|歌曲|制作|製作|歌词|歌詞|翻譯|翻译|插曲)\s*[:：∶]/;
