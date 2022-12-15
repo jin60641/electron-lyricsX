2509b9b32929637b9b081150b84c68ba247194e7
jin60641 <jin60641@naver.com>
2022-12-15 20:59:08 +0900

    fix: 크기측정에 theme 사용하도록 rollback

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -9,6 +9,7 @@ import {
   createStyles,
   makeStyles,
   Theme,
+  useTheme,
 } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { useSelector } from 'react-redux';
@@ -54,6 +55,7 @@ const useStyles = makeStyles<Theme, Props>((theme) => createStyles({
   draggableOff: {
     '-webkit-app-region': 'no-drag',
     '&:hover': { opacity: 0 },
+    transition: 'opacity .3s',
   },
   wrap: {
     position: 'absolute',
@@ -71,20 +73,12 @@ const useStyles = makeStyles<Theme, Props>((theme) => createStyles({
     textShadow: '0px 0px 8px rgba(0,255,255,.68)',
     fontSize: ({ lyricSize }) => lyricSize,
     userSelect: 'none',
-    transition: 'opacity .3s',
     alignItems: 'flex-end',
     flexDirection: 'row',
     opacity: 0,
+    transition: 'opacity .3s',
   },
-  shown: {
-    opacity: 1,
-    padding: `${theme.spacing(1)}px ${theme.spacing(4)}px`,
-    '& ~ &': {
-      marginTop: theme.spacing(-1),
-      paddingTop: 0,
-      paddingBottom: 0,
-    },
-  },
+  shown: { opacity: 1 },
 }));
 
 const selector = ({
@@ -117,6 +111,7 @@ const selector = ({
 });
 
 const Main: React.FC = () => {
+  const theme = useTheme();
   const domRef = useRef<HTMLDivElement | null>(null);
   const [position, setPosition] = useState(0);
   const [time, setTime] = useState(0);
@@ -142,8 +137,7 @@ const Main: React.FC = () => {
   });
   const lyrics = useMemo(() => music?.lyric?.map((lyric, i) => ({
     ...lyric,
-    isSelected: !!lyric.text.length
-      && (lyric.time <= (time + 10))
+    isSelected: (lyric.time <= (time + 10))
       && i < index + lineCount
       && i >= index,
   })), [music?.lyric, time, index, lineCount]);
@@ -179,38 +173,36 @@ const Main: React.FC = () => {
       domRef.current
       && index >= 0
       && lineCount
-      && selectedLyricsCount !== undefined
       && lyricSize
-      && currentOffset !== undefined
-      && globalOffset !== undefined
     ) {
-      const selectedEls = [...domRef.current.children].filter((el) => el.getAttribute('data-selected') === 'true');
-      const windowSize = selectedEls.reduce((obj, selectedEl) => ({
-        ...obj,
-        width: Math.max(obj.width, selectedEl.clientWidth),
-        height: obj.height + selectedEl.clientHeight,
-      }), { width: 0, height: 0 });
-      window.bridge.ipc.send('LAYOUT.RESIZE_WINDOW', windowSize);
+      const selectedEls = [...domRef.current.querySelectorAll('[data-selected=true]')];
+      const windowSize = selectedEls.reduce((prev, selectedEl) => ({
+        width: Math.max(prev.width, selectedEl.clientWidth + theme.spacing(8)),
+        height: prev.height + selectedEl.clientHeight,
+      }), { width: 0, height: theme.spacing(2) });
+      if (selectedLyricsCount) {
+        window.bridge.ipc.send('LAYOUT.RESIZE_WINDOW', windowSize);
+      } else {
+        window.bridge.ipc.send('LAYOUT.RESIZE_WINDOW', { width: 0, height: 0 });
+      }
       if (selectedEls[0]) {
         const [parentTop, childTop] = [domRef.current, selectedEls[0]]
           .map((el) => el.getBoundingClientRect().top);
-        domRef.current.style.top = `${parentTop - childTop}px`;
+        domRef.current.style.top = `${parentTop - childTop + theme.spacing(1)}px`;
       }
     }
   }, [
     domRef,
     index,
     lineCount,
-    currentOffset,
-    globalOffset,
     lyricSize,
     selectedLyricsCount,
+    theme,
   ]);
 
   useEffect(() => {
     // avoid using redux for update immediately
     window.bridge.ipc.receive(getType(musicActions.seekMusic), (data: Music) => {
-      console.log(data);
       if (data.position) {
         setTime(data.position);
         setPosition(data.position);
@@ -218,38 +210,28 @@ const Main: React.FC = () => {
     });
   }, []);
 
-  if (!lyrics?.length) {
-    return null;
-  }
-
   return (
-    <>
+    <div className={clsx(classes.main, { [classes.draggableOff]: !draggable })}>
+      <div className={classes.background} />
       <div
-        className={clsx(classes.main, { [classes.draggableOff]: !draggable })}
+        className={classes.wrap}
+        ref={domRef}
       >
-        <div
-          className={classes.background}
-        />
-        <div
-          className={classes.wrap}
-          ref={domRef}
-        >
-          {lyrics.map(({
-            text,
-            isSelected,
-            id,
-          }) => (
-            <div
-              key={`lyric-row-${id}`}
-              data-selected={isSelected}
-              className={clsx(classes.row, isSelected && classes.shown)}
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{ __html: text }}
-            />
-          ))}
-        </div>
+        {lyrics?.map(({
+          text,
+          isSelected,
+          id,
+        }) => (
+          <div
+            key={`lyric-row-${id}`}
+            data-selected={isSelected}
+            className={clsx(classes.row, isSelected && classes.shown)}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: text }}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
