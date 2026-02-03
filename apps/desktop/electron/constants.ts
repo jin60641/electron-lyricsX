a5264ac53d78680a56d5e4428e61d70c8c6af36f
jin60641 <jin60641@naver.com>
2026-02-03 18:58:53 +0900

    asar 적용

--- apps/desktop/electron/constants.ts
+++ apps/desktop/electron/constants.ts
@@ -3,7 +3,8 @@ import path from 'path';
 
 export const isDev = !app.isPackaged
 export const isMac = process.platform === 'darwin';
-export const resourcePath = isDev ? path.join(__dirname, '..', '..') : process.resourcesPath;
-export const dirPath = isDev ? __dirname : path.join(resourcePath, 'app', 'build','electron');
+const appPath = app.getAppPath();
+export const resourcePath = isDev ? appPath : process.resourcesPath;
+export const dirPath = path.join(appPath, 'build', 'electron');
 export const publicPath = path.join(resourcePath, 'public');
 export const preloadPath = path.join(dirPath, 'preload', 'index.js');
